f5b12312e9890d1ca131aef762b5e063ae695b52
Flávio J. Saraiva <flaviojs2005@gmail.com>
2015-09-28 10:22:32 +0100

    [ADD] exercício 1 de training JMS

---
+++ jms_training/training1/src/Sender.java
@@ -0,0 +1,33 @@
+
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.JMSRuntimeException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class Sender {
+
+    private ConnectionFactory cf;
+    private Destination d;
+
+    public Sender() throws NamingException {
+        this.cf = InitialContext.doLookup("jms/RemoteConnectionFactory");
+        this.d = InitialContext.doLookup("jms/queue/PlayQueue");
+    }
+
+    private void send(String text) {
+        try (JMSContext jcontext = cf.createContext("user", "pass");) {
+            JMSProducer mp = jcontext.createProducer();
+            mp.send(d, text);
+        } catch (JMSRuntimeException re) {
+            re.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws NamingException {
+        Sender s = new Sender();
+        s.send("Hello Receiver!");
+    }
+}
